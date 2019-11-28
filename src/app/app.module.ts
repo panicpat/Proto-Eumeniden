87afb32727599bab40b382bb74b8baa025121d87
panicpat <patrick.richter@rwth-aachen.de>
2019-11-28 12:17:39 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+// only for dog picture
+import {HttpClientModule} from '@angular/common/http';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { RouterModule } from "@angular/router";
+
+import {DemoMaterialModule} from './material-module';
+
+import { AppComponent } from "./app.component";
+import { IntroComponent } from "./intro/intro.component";
+import { EnsembleComponent } from "./ensemble/ensemble.component";
+
+@NgModule({
+  declarations: [AppComponent, IntroComponent, EnsembleComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    DemoMaterialModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "", component: IntroComponent },
+      { path: "intro", component: IntroComponent },
+      { path: "ensemble", component: EnsembleComponent }
+    ])
+  ],
+  exports: [RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/ensemble/ensemble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ensemble',
+  templateUrl: './ensemble.component.html',
+  styleUrls: ['./ensemble.component.css']
+})
+export class EnsembleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
